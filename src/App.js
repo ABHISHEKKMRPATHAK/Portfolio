ad5c368351aad3420264a0823874ff136709df25
Abhishek Kumar Pathak <abhishekkumarpathak@Abhisheks-MacBook-Air.local>
2024-01-04 10:39:41 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import logo from './logo.svg';
+import Header from './components/Header';
+import Welcome from './components/Welcome.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
 import './App.css';
-const greet = ["Hello","Hi","Namaste","Hola"];
-function genRandomInt(max){
-  return Math.floor(Math.random()*(max+1));
-}
-function Header(){
-  const greetIN = greet[genRandomInt(4)]; 
-  return(
-    <h1>{greetIN} Visitor</h1>
-  );
+import reactImg from './assets/pic1.png';
 
-}
-function Welcome(props) {
-  return(
-  <h1>Hello,{props.name}</h1>
-  );
-}
  
-function CoreConcepts(props){
-return(
-  <li>
-    <img src={props.img} alt = {props.alt}  />
-    <h3>{props.til}</h3>
-    <p>{props.des}</p>
-  </li>
-)
-}
+
 function App() {
   return (
  <div>
@@ -34,8 +15,7 @@ function App() {
   <section id ="core-concepts">
     <h2>Core Concepts</h2>
     <ul>
-      <CoreConcepts til="Componemts" des = "Asd jhdksj jdkskj " img="https://www.istockphoto.com/photo/multi-layers-color-texture-3d-papercut-layers-in-gradient-vector-banner-carving-art-gm1573329496-528120566?utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fpic&utm_medium=affiliate&utm_source=unsplash&utm_term=pic%3A%3A%3A"/>
-      <CoreConcepts til="Props"  des ="hgkjh"/>
+      <CoreConcepts title="Componemts" description = "Asd jhdksj jdkskj" img="" />
       <CoreConcepts />
       <CoreConcepts />
     </ul>
