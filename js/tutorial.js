f91b0c4910f013378f3a4a946963ad3a5581a628
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-03-12 08:25:09 +0530

    up

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,2 +1,36 @@
-const name = "Abhishek"
-console.log(`My name is ${name}`);
+/* Variable declaration
+let and const keywords 
+Prefer not to use var because of issue in block scope and functional scope  */
+ 
+                                               /* DATA TYPES */
+
+ // number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniquex
+
+//typeof --> used for getting type of variable
+
+//let name = 'Ramesh'
+//console.log(typeof(name))
+
+                                           /* TYPE CONVERSION */
+
+//let name = "Ram"
+//let num = Number(name)--> converting string to num
+
+// === -->  This operator will check the data type equality also
+                                 
+                                        /* String */
+
+const schoolName = new String("Vivekanand public school")
+//console.log(schoolName[0]);
+//console.log(schoolName.length);
+//console.log(schoolName.charAt(5));
+//console.log(schoolName.indexOf('a'));
+//console.log(schoolName.substring(0,8));
+//let newArr=(schoolName.slice(5,24))
+//console.log(newArr);
