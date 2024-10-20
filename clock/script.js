ef246f9a71a21733171148770f1da1aea35c1bec
Abhishek Pathak <abhishek.pathak2515@gmail.com>
2024-10-20 13:48:42 +0530

    setup

--- clock/script.js
+++
@@ -1,5 +0,0 @@
-const clock = document.getElementById('clock');
-setInterval(()=>{
-let date = new Date();
-clock.innerHTML=date.toLocaleTimeString();
-},1000);
--- number-guessing/script.js
+++
@@ -1,3 +0,0 @@
-setInterval(()=>{
-    console.log(parseInt(Math.random()*10+1))
-},1000)
